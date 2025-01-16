e80a2ec75ed4aa366d23854aef5849f3f51e4eb0
belkud <vlad-belkud@yandex.ru>
2025-01-16 20:49:52 +0300

    gfh

--- js/main.js
+++
@@ -1,5 +0,0 @@
-let mainNav = document.querySelector('.main-nav'), 
-    menuBtn = document.querySelector('.mobile-toggle');;
-menuBtn.addEventListener('click',function(){
-  mainNav.classList.toggle('open');
-})
